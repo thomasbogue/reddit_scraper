0ea2001b191f7cb88d9d3e3c967c96ba5b4c25ca
Tom Bogue <thomasbogue@gmail.com>
2023-11-22 13:25:21 -0500

    added progress messages and loads subreddit list from json

--- leech_reddit.py
+++ leech_reddit.py
@@ -7,41 +7,27 @@ import my_praw
 
 reddit = my_praw.get_reddit()
 
-subreddits = [ "jokes" ]
-N = 1
+subreddit_list_filename = "subreddit_list.json"
+try:
+  subreddits = json.load(open(subreddit_list_filename, "r"))
+except:
+  print(f"error loading {subreddit_list_filename}")
+  exit(1)
+N = 20
 
 texts = []
 
 for subreddit in subreddits:
+  print(f"loading r/{subreddit}                                                                ")
   submissions = reddit.subreddit(subreddit).top(limit=N)
+
   for submission in submissions:
-      print(submission)
-      print(submission.title)
-      pprint.pprint(submission.__dict__,indent=4)
-      pprint.pprint(vars(submission))
+      print(f"loading r/{subreddit}/{submission.title}                              ",end="\r")
       submission.comments.replace_more(limit=2)
       texts.append(submission.selftext)
       for comment in submission.comments.list():
-          pprint.pprint(comment.body)
           texts.append(comment.body)
 
 outfile = open("reddit_texts.json", "w")
 json.dump(texts, outfile)
 outfile.close()
-
-print(len(texts))
-word_counts = {}
-for text in texts:
-    for splitchar in ",;!?@$#":
-        text.replace(splitchar, " ")
-    for word in text.lower().split():
-        if (len(word) >=2):
-            if word in word_counts:
-                word_counts[word] += 1
-            else:
-                word_counts[word] = 1
-
-sorted_word_counts = {k:v for k,v in sorted(word_counts.items(), key=lambda item:item[1])}
-
-for word in sorted_word_counts:
-    print(f"{word}: {word_counts[word]}")
